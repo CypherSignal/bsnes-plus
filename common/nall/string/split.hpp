a11d4c98201427ee71b1b81b675d52de1035a03e
David Crooks <davidacrooks@outlook.com>
2018-11-18 22:33:17 -0500

    Optimization to string split; eliminate accidentally-quadratic behaviour
    -The lstring only has a single reserve occur upfront, instead of reallocations happening multiple times while the lstring grows, eliminating many redundant copies
    -The copying of src string data to the lstring now just uses memcpy instead of strlcpy, which (in this case, redundantly) calculates the length of the string, which would cause the source string to be rescanned many times
    
    On large symbol files (e.g. multiple MBs in size, spanning 100ks of lines that would be split) this accelerates parsing from "Seemingly never finishing" to "Blink and you miss it".

--- common/nall/string/split.hpp
+++ common/nall/string/split.hpp
@@ -8,18 +8,43 @@ template<unsigned Limit> void lstring::split(const char *key, const char *src) {
   reset();
 
   int ssl = strlen(src), ksl = strlen(key);
-  int lp = 0, split_count = 0;
+  int lp = 0;
 
-  for(int i = 0; i <= ssl - ksl;) {
-    if(!memcmp(src + i, key, ksl)) {
-      strlcpy(operator[](split_count++), src + lp, i - lp + 1);
+  // scan through the src string and find how many and where all of the splits should occur
+  linear_vector<const char*> splitLocations;
+  linear_vector<unsigned int> splitLengths;
+  for(int i = 0; i <= ssl - ksl;)
+  {
+    if(!memcmp(src + i, key, ksl))
+    {
+      splitLocations.append(src + lp);
+      splitLengths.append(i - lp);
       i += ksl;
       lp = i;
-      if(!--limit) break;
-    } else i++;
+      if (!--limit)
+      {
+        break;
+      }
+    } 
+    else
+    {
+      i++;
+    }
   }
 
-  operator[](split_count++) = src + lp;
+  // do a single reserve of the entire lstring, and copy the contents of each string one-at-a-time
+  resize(splitLocations.size()+1);
+  for (int i = 0; i < splitLocations.size(); ++i)
+  {
+    string& str = operator[](i);
+    unsigned int splitLength = splitLengths[i];
+    
+    str.reserve(splitLength);
+    memcpy((void*)str(), (void*)splitLocations[i], splitLength);
+    str[splitLength] = 0;
+  }
+
+  operator[](splitLocations.size()) = src + lp;
 }
 
 template<unsigned Limit> void lstring::qsplit(const char *key, const char *src) {
